500fb2b5b4b1286c8f2846037f4e848c4002a5e2
encryptinator <hashrabbit@yahoo.com>
2017-03-26 15:35:18 +0200

    Create Hellow.java

---
+++ Hellow.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Hellow
+{
+     public static void main()
+     {
+         String name;
+         Scanner in = new Scanner(System.in);
+         System.out.println("Hellow what is you name ");
+         name = in.next();
+         in.close();
+         System.out.println("hellow "+name);
+      }
+ )
